91e161608657b16ba2cce9478be365d10fbd2607
pavel-malin <kurchevskijpavel@gmail.com>
2020-12-24 16:29:39 +0300

    new File

---
+++ GameStats3/GameStats3/game_stats3.cpp
@@ -0,0 +1,18 @@
+// Games Stats3.0
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	const int ALIEN_POINTS = 150;
+	int aliensKilled = 10;
+	int score = aliensKilled * ALIEN_POINTS;
+	cout << "score: " << score << endl;
+	enum difficulty {NOVICE ,EASY, NORMAL, HARD, UNBEATABLE};
+	difficulty myDifficulty = EASY;
+	enum shipCost {FIGHTER_COST = 25, BOMBOER_COST, CRUISER_COST = 50};
+	shipCost ShipCost = BOMBOER_COST;
+	cout << "\nTo upgrade my ship to a Cruiser will cost" << (CRUISER_COST - ShipCost) << " Resource Points.\n";
+	return 0;
+}
