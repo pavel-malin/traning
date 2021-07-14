2613c3c66dc532d3d1adb72dcd778f56e4de1dd6
pavel-malin <kurchevskijpavel@gmail.com>
2021-07-14 13:06:07 -0700

    new file 6.7

---
+++ ENGCONST/ENGCONST/engConst.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Distance
+{
+private:
+	int feet;
+	float inches;
+public:
+	Distance() : feet(0), inches(0.0)
+	{}
+	Distance(int ft, float in) : feet(ft), inches(in)
+	{}
+	void getdist()
+	{
+		cout << "\nEnter the number of feet: ";
+		cin >> feet;
+		cout << "Enter the number of inches: ";
+		cin >> inches;
+	}
+	void showdist()const
+	{
+		cout << feet << "\'-" << inches << '\"';
+	}
+	Distance add_dist(const Distance&) const;
+};
+
+Distance Distance::add_dist(const Distance& d2) const
+{
+	Distance temp;
+	temp.inches = inches + d2.inches;
+	if (temp.inches >= 12.0)
+	{
+		temp.inches -= 12.0;
+		temp.feet = 1;
+	}
+	temp.feet += feet + d2.feet;
+	return temp;
+}
+
+int main()
+{
+	Distance dist1, dist3;
+	Distance dist2(11, 6.25);
+	dist1.getdist();
+	dist3 = dist1.add_dist(dist2);
+	cout << "\ndist1 = ";
+	dist1.showdist();
+	cout << "\ndist2 = ";
+	dist2.showdist();
+	cout << "\ndist3 = ";
+	dist3.showdist();
+	cout << endl;
+	return 0;
+}
