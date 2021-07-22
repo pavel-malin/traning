64858cb0ac1d313f0139d4bd5e59aa570f7c7c75
pavel-malin <kurchevskijpavel@gmail.com>
2021-07-22 04:37:47 -0700

    new files 7.6

--- SALEFUNC/SALEFUNC/salefunc.cpp
+++ SALEFUNC/SALEFUNC/salefunc.cpp
@@ -4,18 +4,18 @@
 using std::cout;
 using std::endl;
 
-using std::ios;
 using std::setw;
+using std::ios;
 using std::setiosflags;
 using std::setprecision;
 
 const int DISTRICTS = 4;
 const int MONTHS = 3;
 
+void display(double[DISTRICTS][MONTHS]);
+
 int main()
 {
-	int d, m;
-
 	double sales[DISTRICTS][MONTHS] =
 	{
 		{1432.07, 234.50, 654.01},
@@ -23,18 +23,27 @@ int main()
 		{9328.34, 934.00, 4492.30},
 		{12838.29, 2332.63, 32.93}
 	};
-	cout << "\n\n";
-	cout << "Month\n";
-	cout << "		1	  2	   3";
-	for (d = 0; d < DISTRICTS; d++)
-	{
-		cout << "\nDepartment " << d + 1;
-		for (m = 0; m < MONTHS; m++)
-		{
-			cout << setw(10) << setiosflags(ios::fixed) << setiosflags(ios::showpoint)
-				<< setprecision(2) << sales[d][m];
-		}
-	}
+
+	display(sales);
 	cout << endl;
 	return 0;
+}
+
+void display(double funsales[DISTRICTS][MONTHS])
+{
+	int d, m;
+	cout << "\n\n";
+	cout << "Month\n";
+	cout << "		   1		2	  3";
+	
+	for (d = 0; d < DISTRICTS; d++)
+	{
+		cout << "\nDepartment: " << d + 1;
+		for (m = 0; m < MONTHS; m++)
+		{
+			cout << setiosflags(ios::fixed) << setw(10)
+				<< setiosflags(ios::showpoint) << setprecision(2)
+				<< funsales[d][m];
+		}
+	}
 }
