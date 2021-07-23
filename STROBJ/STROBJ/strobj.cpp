eb01ea19b9ce24f125e05b89ef99844fe0d12937
pavel-malin <kurchevskijpavel@gmail.com>
2021-07-23 08:01:51 -0700

    new files 7.20(error files c2664, 2440)

---
+++ STROBJ/STROBJ/strobj.cpp
@@ -0,0 +1,53 @@
+#pragma warning(disable : 4996) // not errror strcpy();
+#include<iostream>
+#include<cstring>
+
+using std::cout;
+using std::endl;
+
+using std::strcpy;
+
+class String
+{
+private:
+	enum { SZ = 80 };
+	char str[SZ];
+public:
+	String()
+	{
+		str[0] = '\0';
+	}
+	String(char s[])
+	{
+		strcpy(str, s);
+	}
+	void display()
+	{
+		cout << str;
+	}
+	void concat(String s2)
+	{
+		if (strlen(str) + strlen(s2.str) < SZ)
+		{
+			strcat(str, s2.str);
+		}
+		else
+		{
+			cout << "\nOverflow!";
+		}
+	}
+};
+
+int main()
+{
+	String s1( "Happy New Year! " ); // error programming c2664
+	String s2 = "With new happiness!"; // error programming c2440
+	String s3;
+	cout << "\ns1=";
+	s1.display();
+	cout << "\ns2=";
+	s2.display();
+	cout << "\ns3=";
+	s3.display();
+	return 0;
+}
