a470c8e42daa8e85bac4aea3fabbf8d3c7e7b091
pavel-malin <kurchevskijpavel@gmail.com>
2021-07-23 00:41:48 -0700

    new files 7.11

---
+++ STRINGIN/STRINGIN/stringin.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	const int MAX = 80;
+	char str[MAX];
+	cout << "Enter the stirng: ";
+	cin >> str;
+	cout << "You enter: " << str << endl;
+	return 0;
+}
