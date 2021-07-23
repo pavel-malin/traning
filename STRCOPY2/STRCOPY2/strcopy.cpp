1d1a5fa9c447afc5c1c0e855c502eec174bd4078
pavel-malin <kurchevskijpavel@gmail.com>
2021-07-23 02:25:58 -0700

    new files 7.17

---
+++ STRCOPY2/STRCOPY2/strcopy.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+#pragma warning(disable : 4996)
+
+using std::cout;
+using std::endl;
+
+using std::strcpy; // error
+
+int main()
+{
+	char str1[] = "They dropped the bear on the floor, tore off the bear's paw!";
+	const int MAX = 80;
+	char str2[MAX];
+	strcpy(str2, str1); // error
+	cout << str2 << endl;
+	return 0;
+}
