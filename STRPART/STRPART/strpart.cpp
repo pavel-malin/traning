8d481fb768bf76c40ee9f7f4c55482fcb26b3a80
pavel-malin <kurchevskijpavel@gmail.com>
2021-07-23 07:22:04 -0700

    new files 7.19(error files)

---
+++ STRPART/STRPART/strpart.cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable : 4996) // not errror strcpy();
+#include<iostream>
+#include<cstring>
+
+using std::cout;
+using std::endl;
+
+using std::strcpy;
+
+
+class part
+{
+private:
+	char partname[30];
+	int partnumber;
+	double cost;
+public:
+	void setpart(char pname[], int pn, double c)
+	{
+		strcpy(partname, pname);
+		partnumber = pn;
+		cost = c;
+	}
+	void showpart()
+	{
+		cout << "\nName=" << partname;
+		cout << ", number=" << partnumber;
+		cout << ", cost=$" << cost;
+	}
+};
+
+int main()
+{
+	part part1, part2;
+	part1.setpart("clutch", 4473, 217.55); // error	c2664
+	part2.setpart("crank", 9924, 419.25);  // error c2664
+	cout << "\nOne detail: ";
+	part1.showpart();
+	cout << "\nTwo detail: ";
+	part2.showpart();
+	return 0;
+}
