63a07b7ce04d53d2e9fdc6ab49e0e2f7d4fc63b0
pavel-malin <kurchevskijpavel@gmail.com>
2021-08-04 00:40:04 -0700

    new files 8.4

---
+++ POSTFIX/POSTFIX/postfix.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+class Counter
+{
+private:
+	unsigned int count;
+public:
+	Counter() :count(0)
+	{}
+	Counter(int c) : count(c)
+	{}
+	unsigned int get_count()
+	{
+		return count;
+	}
+	Counter operator++()
+	{
+		return Counter(++count);
+	}
+	Counter operator++(int)
+	{
+		return Counter(count++);
+	}
+};
+
+int main()
+{
+	Counter c1, c2;
+
+	cout << "\nc1 = " << c1.get_count();
+	cout << "\nc2 = " << c2.get_count();
+
+	++c1;
+	c2 = ++c1;
+
+	cout << "\nc1 = " << c1.get_count();
+	cout << "\nc2 = " << c2.get_count();
+
+	c2 = c1++;
+
+	cout << "\nc1 = " << c1.get_count();
+	cout << "\nc2 = " << c2.get_count() << endl;
+
+	return 0;
+}
