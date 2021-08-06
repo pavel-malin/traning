a91f0bb801ac97a2a0b19d7546d741edeeb0e452
pavel-malin <kurchevskijpavel@gmail.com>
2021-08-06 12:24:40 -0700

    new files 8.14(ERROR C4996 and C2440)

---
+++ STRCONV/STRCONV/strcovn.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string.h>
+
+using std::cout;
+using std::endl;
+
+using std::strcpy;
+
+class String
+{
+private:
+	enum { SZ = 80 };
+	char str[SZ];
+public:
+	String()
+	{
+		str[0] = '\x0';
+	}
+	String(char s[])
+	{
+		strcpy(str, s); // ERROR C4996
+	}
+	void display() const
+	{
+		cout << str;
+	}
+	operator char* ()
+	{
+		return str;
+	}
+};
+
+int main()
+{
+	String s1;
+
+	char xstr[] = "Hurray, comrades!";
+	s1 = xstr;
+
+	s1.display();
+	
+	String s2 = "We will win!"; // ERROR C2440
+	cout << static_cast<char*>(s2);
+	cout << endl;
+
+	return 0;
+}
