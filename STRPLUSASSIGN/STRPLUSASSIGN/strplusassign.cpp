6ad9e9dbeffe4fd0e98e16bbf51e95e1f77613a0
pavel-malin <kurchevskijpavel@gmail.com>
2021-08-12 02:15:55 -0700

    new files 8.p2

---
+++ STRPLUSASSIGN/STRPLUSASSIGN/strplusassign.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<string.h>
+#include<stdlib.h>
+
+using std::cout;
+using std::endl;
+
+using std::strcpy;
+using std::strcat;
+
+using std::exit;
+
+
+class String
+{
+private:
+	enum { SZ = 80 };
+	char str[SZ];
+public:
+	String()
+	{
+		strcpy_s(str, "");
+	}
+	String(char s[])
+	{
+		strcpy_s(str, s);
+	}
+	void display()
+	{
+		cout << str;
+	}
+	String operator+=(String ss) const
+	{
+		String temp;
+		if (strlen(str) + strlen(ss.str) < SZ)
+		{
+			strcpy_s(temp.str, str);
+			strcat_s(temp.str, ss.str);
+		}
+		else
+		{
+			cout << "\nOveflow!";
+			exit(1);
+		}
+		return temp;
+	}
+
+};
+
+int main()
+{
+	char ss1[] = "\nMerry Christmas!";
+	String s1 = ss1;
+	s1.display();
+
+	char ss2[] = "Happy New Year!";
+	String s2 = ss2;
+	s2.display();
+
+	String s3;
+	s3.display();
+	s3 = s1 += s2;
+	s3.display();
+
+	return 0;
+}
