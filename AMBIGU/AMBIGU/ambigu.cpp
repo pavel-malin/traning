2e6d939da7640ad7310ffdde77d3e6f7b4400391
pavel-malin <kurchevskijpavel@gmail.com>
2021-08-18 01:37:45 -0700

    new files 9.10

---
+++ AMBIGU/AMBIGU/ambigu.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using std::cout;
+
+class A
+{
+public:
+	void show()
+	{
+		cout << "Class A\n";
+	}
+};
+
+class B
+{
+public:
+	void show()
+	{
+		cout << "Class B\n";
+	}
+};
+
+class C : public A, public B
+{};
+
+int main()
+{
+	C objC;
+	// objC.show(); ERROR
+	objC.A::show();
+	objC.B::show();
+	return 0;
+}
