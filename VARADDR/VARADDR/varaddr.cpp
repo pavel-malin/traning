f8eeb37fa4e6329b209a19a16c69f4f2ebf765bf
pavel-malin <kurchevskijpavel@gmail.com>
2021-08-27 10:54:16 -0700

    new files 10.1

---
+++ VARADDR/VARADDR/varaddr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int var1 = 11;
+	int var2 = 22;
+	int var3 = 33;
+
+	cout << &var1 << endl
+		<< &var2 << endl
+		<< &var3 << endl;
+	
+	return 0;
+}
