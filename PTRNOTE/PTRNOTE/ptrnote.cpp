eb2ec2ec0f7352a4178768f23f19826363a18491
pavel-malin <kurchevskijpavel@gmail.com>
2021-08-30 04:11:28 -0700

    new files 10.7

---
+++ PTRNOTE/PTRNOTE/ptrnote.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int intarray[5] = { 31,54,77,52,93 };
+	
+	for (int i = 0; i < 5; i++)
+	{
+		cout << *(intarray + i) << endl;
+	}
+
+	return 0;
+}
