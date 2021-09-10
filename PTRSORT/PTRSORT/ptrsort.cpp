4b9a7e21c120ae0a6dbafb229f493bd95dca61ae
pavel-malin <kurchevskijpavel@gmail.com>
2021-09-10 02:43:53 -0700

    new files 10.12

---
+++ PTRSORT/PTRSORT/ptrsort.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	void bsort(int*, int);
+	const int N = 10;
+	int arr[N] = { 37,84,62,91,11,65,57,28,19,49 };
+
+	bsort(arr, N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
+
+void bsort(int* ptr, int n)
+{
+	void order(int*, int*);
+	int j, k;
+	for (int j = 0; j < n - 1; j++)
+	{
+		for (int k = j + 1; k < n; k++)
+		{
+			order(ptr + j, ptr + k);
+		}
+	}
+}
+
+void order(int* numb1, int* numb2)
+{
+	if (*numb1 > *numb2)
+	{
+		int temp = *numb1;
+		*numb1 = *numb2;
+		*numb2 = temp;
+	}
+}
