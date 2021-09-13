1b95204075403f24f8a1747375fb4386b471512b
pavel-malin <kurchevskijpavel@gmail.com>
2021-09-13 12:59:52 -0700

    new files  10.17strcpy_s C2660 ERROR

---
+++ NEWWINTRO/NEWWINTRO/newintro.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstring>
+
+using std::cout;
+using std::endl;
+
+using std::strlen;
+using std::strcpy;
+
+int main()
+{
+	char s[] = "Bad head does not give rest to the legs.";
+	char* str = s;
+	int len = strlen(str);
+
+	char* ptr;
+	ptr = new char[len + 1];
+	strcpy_s(ptr, str); // defin (ERROR C2660) _CRT_SECURE_NO_WARNINGS
+
+	cout << "ptr = " << ptr << endl;
+
+	delete[] ptr;
+
+	return 0;
+	
+}
