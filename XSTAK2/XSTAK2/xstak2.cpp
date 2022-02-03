0b6c350bea468a0a69ef7c2efce5146b01615b0f
Павел Курчевский <kurchevskijpavel@gmail.com>
2022-02-03 11:41:23 +0300

    new files 14.8

---
+++ XSTAK2/XSTAK2/xstak2.cpp
@@ -0,0 +1,62 @@
+// demonstrates two exception handlers
+#include<iostream>
+
+using namespace std;
+
+const int MAX = 3;   // stack hold 3 integers
+
+class Stack
+{
+private:
+	int st[MAX];  // stack: array of integers
+	int top;   // index of top of stack
+public:
+	class Full	// exception class
+	{};
+	class Empty  // exception class
+	{};
+
+	Stack()  // constructor
+	{
+		top = -1;
+	}
+	
+	void push(int var)  // put number on stack
+	{
+		if (top >= MAX - 1)  // if stack full,
+			throw Full();   // throw Full exception
+		st[++top] = var;
+	}
+	int pop()   // take number off  stack
+	{
+		if (top < 0)   // if stack empty,
+			throw Empty(); // throw Empty exception
+		return st[top--];
+	}
+};
+
+int main()
+{
+	Stack s1;
+
+	try
+	{
+		s1.push(11);
+		s1.push(22);
+		s1.push(33);
+//		s1.push(44);     // oops: stack full
+		cout << "1: " << s1.pop() << endl;
+		cout << "2: " << s1.pop() << endl;
+		cout << "3: " << s1.pop() << endl; // oops: stack empty
+		cout << "4: " << s1.pop() << endl;
+	}
+	catch (Stack::Full)
+	{
+		cout << "Exception: Stack Full" << endl;
+	}
+	catch (Stack::Empty)
+	{
+		cout << "Exception: Stack Emply" << endl;
+	}
+	return 0;
+}
