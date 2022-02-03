7754baf46f7152d9dc6fd01b5eb2a369dd4e1545
Павел Курчевский <kurchevskijpavel@gmail.com>
2022-02-03 21:56:58 +0300

    new files 15.4

---
+++ SEARCH/SEARCH/search.cpp
@@ -0,0 +1,20 @@
+// serches one container for a sequence in another container
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int source[] = { 11, 44, 33,11,22,33,11,22,44 };
+int pettern[] = { 11,22,33 };
+
+int main()
+{
+	int* ptr;
+	ptr = search(source, source + 9, pettern, pettern + 3);
+
+	if (ptr == source + 9)			// if past-the-end
+		cout << "No match found\n";
+	else
+		cout << "Match at " << (ptr - source) << endl;
+	return 0;
+}
