df993264786f361d6fe81e8a976dce3ad2f429e1
Павел Курчевский <kurchevskijpavel@gmail.com>
2022-02-03 13:34:57 +0300

    new files 14.9

---
+++ xdist/xdist/xdist.cpp
@@ -0,0 +1,62 @@
+// exceptions with Distance class
+
+#include<iostream>
+
+using namespace std;
+
+class Distance   // English Distance class
+{
+private:
+	int feet;
+	float inches;
+public:
+	class InchesEx   // enception class
+	{};
+	Distance()  // costructor (no args)
+	{
+		feet = 0;
+		inches = 0.0;
+	}
+	Distance(int ft, float in)   // constructor (two args)
+	{
+		if (in >= 12.0)			// if inches too big,
+			throw InchesEx();   // throw exception
+		feet = ft;
+		inches = in;
+	}
+	void getdist()   // get length from user
+	{
+		cout << "\nEnter feet: ";
+		cin >> feet;
+		cout << "Enter inches: ";
+		cin >> inches;
+		if (inches >= 12.0)   // if inches too big,
+			throw InchesEx();  // throw exception
+	}
+	void showdist()   // display distance
+	{
+		cout << feet << "\'-" << inches << '\"';
+	}
+};
+
+int main()
+{
+	try
+	{
+		Distance dist1(17, 3.5);  // 2-arg constructor
+		Distance dist2;  // no-arg constructor
+		dist2.getdist();  // get distance from user
+						  // display distnaces
+		cout << "\ndist1 = ";
+		dist1.showdist();
+		cout << "\ndist2 = ";
+		dist2.showdist();
+	}
+	catch (Distance::InchesEx)  // catch exceptions
+	{
+		cout << "\nInstialization error "
+			"inches value is too large.";
+	}
+	cout << endl;
+	return 0;
+}
