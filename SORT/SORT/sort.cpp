7d1f3823f3e709add05051e31d83eb1307292866
Павел Курчевский <kurchevskijpavel@gmail.com>
2022-02-03 21:42:29 +0300

    new files 15.3

---
+++ SORT/SORT/sort.cpp
@@ -0,0 +1,18 @@
+// sorts an array of integer
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+// array of numbers
+int arr[] = { 45, 2, 22, -17, 0, -30, 25, 55 };
+
+int main()
+{
+	sort(arr, arr + 8);  // sort the numbers
+
+	for (int j = 0; j < 8; j++)  // display sorted array
+		cout << arr[j] << ' ';
+	cout << endl;
+	return 0;
+}
