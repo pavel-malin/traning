b697f6aadd7b7ce20710142fc4b064bc33166f8b
Павел Курчевский <kurchevskijpavel@gmail.com>
2022-02-08 11:09:40 +0300

    new files 15.23

---
+++ OUTITER/OUTITER/outiter.cpp
@@ -0,0 +1,22 @@
+// demonstrates ostream_iterator
+#include<iostream>
+#include<algorithm>
+#include<list>
+
+using namespace std;
+
+int main()
+{
+	int arr[] = { 10,20,30,40,50 };
+	list<int> theList;
+
+	for (int j = 0; j < 5; j++)  // transfer array to list
+		theList.push_back(arr[j]);
+
+	ostream_iterator<int> ositer(cout, ", ");  // ostream oterator
+
+	cout << "\nContents of list: ";
+	copy(theList.begin(), theList.end(), ositer);  // display list
+	cout << endl;
+	return 0;
+}
