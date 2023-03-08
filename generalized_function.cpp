7480a03ee93b394ada7e1d2c9f30a9178b750058
malinroot <24213684+pavel-malin@users.noreply.github.com>
2023-03-08 12:47:37 +0300

    Add files via upload

---
+++ generalized_function.cpp
@@ -0,0 +1,11 @@
+template <typename T>
+T add(T x, T y, T x) {
+	return x+y+z;
+}
+
+int main() {
+	auto a = add(1, 2, 3); // a has type int
+	auto b = add(1L, 2L, 3L); // b has type long
+	auto c = add(1.F, 2.F, 3.F) // c has type float
+}
+
